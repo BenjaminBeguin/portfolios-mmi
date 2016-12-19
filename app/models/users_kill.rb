a99679968c37cbb4cdf70d51930ee2cf2c93a25b
Benjamin Beguin <beguin.ben@gmail.com>
2016-12-19 16:28:46 +0100

    create controllers

--- app/models/users_kill.rb
+++
@@ -1,2 +0,0 @@
-class UsersKill < ApplicationRecord
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'skills/index'
+
+  get 'skills/new'
+
+  get 'jobs/index'
+
+  get 'jobs/new'
+
+  get 'portfolios/index'
+
+  get 'users/show'
+
+  get 'users/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
