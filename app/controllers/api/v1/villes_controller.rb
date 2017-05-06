f35d151e401fd46a53eb245d4c97ca50888c80ab
Benjamin Beguin <beguin.ben@gmail.com>
2017-05-06 20:27:10 +0200

    fix route

--- app/controllers/api/v1/villes_controller.rb
+++ app/controllers/api/v1/villes_controller.rb
@@ -2,9 +2,26 @@ class Api::V1::VillesController < Api::V1::BaseController
 
 	api :GET, "/", "Show all posts"
 	def index
-	    cities = Ville.where(have_portfolio: true).order(like: :desc)
-			render json: cities
+	    # cities = Ville.where(have_portfolio: true).order(like: :desc)
+			# @portfolios = Ville.joins(:user).where(users: {ville_id: ids).page(params[:page]).per(PORTFOLIO_PER_PAGE);
+			# render json: @portfolios
+
+			portfolios = Portfolio.all.published;
+
+			ville_with_portfolio_portfolio = []
+			ville_with_portfolio_portfolio_id = []
+
+			portfolios.each do |portfolio|
+				ville = Ville.where(id: portfolio.user.ville.id).first
+				if !ville_with_portfolio_portfolio_id.include? ville.id
+					ville_with_portfolio_portfolio << ville
+					ville_with_portfolio_portfolio_id << ville.id
+				end
+			end
+
+		 	render json: ville_with_portfolio_portfolio
 	end
+
 
 	def all
 	    cities = Ville.order(:label)
